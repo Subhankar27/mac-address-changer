467c1fd39f249c2469db172d60238b4dd1f0cf31
Subhankar Pati <82767744+Subhankar27@users.noreply.github.com>
2022-01-15 16:56:58 +0530

    Update mac_address_changer.py

--- mac_address_changer.py
+++ mac_address_changer.py
@@ -25,6 +25,7 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
+    
 
 
 options = get_arguments()
