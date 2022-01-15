c0a93b7d3484c28dae7799f5a669df9d4dcfb840
Subhankar Pati <82767744+Subhankar27@users.noreply.github.com>
2022-01-15 16:56:13 +0530

    Update mac_address_changer.py

--- mac_address_changer.py
+++ mac_address_changer.py
@@ -8,6 +8,7 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface", help="interface to change mac address")
     parser.add_option("-m", "--mac", dest="new_mac", help="new mac address")
     (options, arguments) = parser.parse_args()
+    
     if not options.interface:
         parser.error("[-] Please spicify an interface , use --help for more info.")
     elif not options.new_mac:
